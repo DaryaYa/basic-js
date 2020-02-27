d4d9efa1e66bc7e762a3caeadbea6a77e3933ce5
DaryaYa <lyalyaka@mail.ru>
2020-02-27 20:40:41 +0300

    feat: solved

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,17 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+let activity = 0;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+   
+if(typeof(sampleActivity) == 'string') {
+  activity = parseFloat(sampleActivity);
+} else {
+  return false;
+}
+
+if (activity > 0 && activity <= MODERN_ACTIVITY) {
+  return Math.ceil(Math.log(MODERN_ACTIVITY / activity) * HALF_LIFE_PERIOD / 0.693)} 
+
+ return false;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    
+    if(!(Array.isArray(arr))) {throw new Error()}
+    let array = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "--discard-next") {
+            i++;
+        } else if (arr[i] === "--discard-prev") {
+            array.pop();           
+        
+        } else if (arr[i] === "--double-next" && i === arr.length-1) {
+            i++;
+        } else if (arr[i] === "--double-next") {           
+            array.push(arr[i+1]);
+        } else if (arr[i] === "--double-prev" && i == 0) {
+            i == i;
+        }
+        else if (arr[i] === "--double-prev") { 
+            array.push(arr[i - 1])           
+        } else {
+            array.push(arr[i]);
+        }
+    }
+
+    return array;
 };
