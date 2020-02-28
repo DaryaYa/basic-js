687631a99d8edaac19aef60e484ecc38a038046b
DaryaYa <lyalyaka@mail.ru>
2020-02-28 20:21:45 +0300

    more tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,30 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    
+    let string ='';
+
+    let {
+        repeatTimes = 1,
+        separator = '+',
+        addition = (options.addition === undefined) ? '' : options.addition + '',
+        additionRepeatTimes = 1,
+        additionSeparator = '|',    
+    } = options;
+
+    this.options = options;
+
+    if(typeof(str) !== 'string') {
+        str = String(str);
+    }
+
+    for(let i = repeatTimes; i > 0 ; i-= 1) {
+        string += str; 
+        for(let j = additionRepeatTimes; j > 0 ; j-= 1) {
+            string += addition;
+            if (j > 1) {string+= additionSeparator;}
+        } 
+        if (i > 1) {string += separator;}
+    }
+
+    return string;
 };
   
