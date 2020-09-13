19b7a5a89e505f66a7473b742b2e64411682d92a
DaryaYa <lyalyaka@mail.ru>
2020-09-13 19:23:32 +0300

    fix: updated tasks

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,7 +1,7 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
 
     let num = Math.pow(2, disksNumber) - 1;
-    let secs = num/ (turnsSpeed/3600);
+    let secs = Math.floor(num/ (turnsSpeed/3600)) ;
     
     return {turns: num, seconds: secs};
    
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,25 +3,41 @@ module.exports = function transform(arr) {
     if(!(Array.isArray(arr))) {throw new Error()}
     let array = [];
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === "--discard-next") {
-            i++;
-        } else if (arr[i] === "--discard-prev") {
-            array.pop();           
-        
-        } else if (arr[i] === "--double-next" && i === arr.length-1) {
-            i++;
-        } else if (arr[i] === "--double-next") {           
-            array.push(arr[i+1]);
-        } else if (arr[i] === "--double-prev" && i == 0) {
-            i == i;
+   for (let i = 0; i < arr.length; i++){
+        switch(arr[i]) {
+            case '--discard-next': {
+                if(i + 1 < arr.length){
+                    i++;
+                };
+                break;
+            }
+
+            case '--discard-prev': {
+                if(i - 1 >= 0 && arr[i - 2] !== '--discard-next'){
+                    array.pop();
+                };
+                break;
+            }
+
+            case '--double-next': {
+                if(i + 1 < arr.length){
+                    array.push(arr[i + 1]);
+                };
+                break;
+            }
+
+            case '--double-prev': {
+                if(i - 1 >= 0 && arr[i - 2] !== '--discard-next'){
+                    array.push(arr[i - 1]);
+                };
+                break;
+            }
+
+            default: {
+                array.push(arr[i]);   
+            }
         }
-        else if (arr[i] === "--double-prev") { 
-            array.push(arr[i - 1])           
-        } else {
-            array.push(arr[i]);
-        }
-    }
+    };
 
     return array;
 };
